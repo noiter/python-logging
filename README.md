da5bd97da6ab7b5514a531a266c325b3b5f15ef6
noiter <zm_uestc@hotmail.com>
2018-02-26 17:29:03 +0800

    Init repo for python logging practise

---
+++ README.md
@@ -0,0 +1,2 @@
+### How to run
+LOG_CFG=<PATH_TO_LOGGING_CONFIG_FILE> python main.py
---
+++ main.py
@@ -0,0 +1,14 @@
+import logging
+
+import logging_settings
+import test_module
+
+def main():
+    test_module.foo()
+    bar = test_module.Bar()
+    bar.bar()
+
+if __name__ == '__main__':
+    logging_settings.configure()
+
+    main()
